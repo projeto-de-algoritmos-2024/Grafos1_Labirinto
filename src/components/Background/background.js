07b658b871bd0c4649169c52de4bb206c21fbabc
Luanmq <luanmelo.q@gmail.com>
2024-04-15 10:31:31 -0300

    adicao de novas imagens

--- src/components/Background/background.js
+++ src/components/Background/background.js
@@ -8,8 +8,10 @@ import TrasFrente from "./assets/tras-frente.svg";
 import TrasFrenteDir from "./assets/tras-frente-dir.svg";
 import TrasFrenteEsq from "./assets/tras-frente-esq.svg";
 import TrasFrenteEsqDir from "./assets/tras-frente-esq-dir.svg";
+import TrasDirEsq from "./assets/tras-dir-esq.svg";
+import TrasDir from "./assets/tras-dir.svg";
+import TrasEsq from "./assets/tras-esq.svg";
 
-import { Link } from "react-router-dom";
 import "../../pages/menu/menu.css";
 export default function Background(props) {
   // Objeto que mapeia os valores de props.text para os caminhos dos arquivos SVG correspondentes
@@ -20,6 +22,9 @@ export default function Background(props) {
     TrasFrenteDir: TrasFrenteDir,
     TrasFrenteEsq: TrasFrenteEsq,
     TrasFrenteEsqDir: TrasFrenteEsqDir,
+    TrasDirEsq: TrasDirEsq,
+    TrasDir: TrasDir,
+    TrasEsq: TrasEsq,
   };
 
   // Verifica se a propriedade text existe nos caminhos SVG
@@ -43,5 +48,8 @@ Background.propTypes = {
     "TrasFrenteDir",
     "TrasFrenteEsq",
     "TrasFrenteEsqDir",
+    "TrasDirEsq",
+    "TrasDir",
+    "TrasEsq",
   ]).isRequired,
 };
